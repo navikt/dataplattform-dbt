d60c6518dba68cca17473cb5774a43b721682d78
vebjorre <vebjorn.rekkebo@nav.no>
2026-05-20 13:50:29 +0200

    Create first semantic model

---
+++ models/metrics/time_spine_daily.sql
@@ -0,0 +1,28 @@
+{{
+    config(
+        materialized='table',
+    )
+}}
+
+with
+
+base_dates as (
+    {{
+        dbt.date_spine(
+            'day',
+            "DATE('2000-01-01')",
+            "DATE('2030-01-01')"
+        )
+    }}
+),
+
+final as (
+    select
+        cast(date_day as date) as date_day
+    from base_dates
+)
+
+select *
+from final
+where date_day > DATE_SUB(CURRENT_DATE(), INTERVAL 5 YEAR)
+  and date_day < DATE_ADD(CURRENT_DATE(), INTERVAL 30 DAY)
